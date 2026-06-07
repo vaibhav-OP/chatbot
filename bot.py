ff6b68046aae467bce2f1dec1f5738bd7bf08a11
vaibhav-OP <vaibhavyash28@gmail.com>
2026-06-07 14:29:54 +0530

    Refactor embedding generation and update confidence threshold in chatbot response

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import json
 import os
+import uvicorn
 
 with open("data.json", "r") as f:
     data = json.load(f)
@@ -10,25 +11,31 @@ with open("data.json", "r") as f:
 questions = [item["questions"] for item in data]
 answers = [item["answers"] for item in data]
 
-embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+embedding_model = SentenceTransformer('all-mpnet-base-v2')
 
 cached_embedding_filename = "embeddings.npy"
 
 if not os.path.isfile(cached_embedding_filename):
     print("------------------Generating embeddings...------------------")
-    embeddings = embedding_model.encode(questions)
+    
+    search_text = [
+        f"{item['questions']} {item['answers']}"
+        for item in data
+    ]
+    
+    embeddings = embedding_model.encode(search_text, normalize_embeddings=True)
     np.save(cached_embedding_filename, embeddings)
 question_embeddings = np.load(cached_embedding_filename)
 
 def ask_chatbot(question):
-  query_embedding = embedding_model.encode([question])
+  query_embedding = embedding_model.encode([question], normalize_embeddings=True)
 
   similarities = cosine_similarity(query_embedding, question_embeddings)
   best_match_index = np.argmax(similarities)
 
   confidence = similarities[0][best_match_index]
   
-  if confidence < 0.6:
+  if confidence < 0.4:
     return {
         "answer": "I dont understand the question :(",
         "confidence": float(confidence)
